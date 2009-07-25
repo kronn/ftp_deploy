efda7eb3fa5a55caf81acafed4421d1599161e6a
Matthias Viehweger <kronn@kronn.de>
2009-07-25 18:37:15 +0200

    added a stub of how I wished it would work

--- lib/ftp_deploy.rb
+++ lib/ftp_deploy.rb
@@ -1,13 +1,28 @@
 require 'net/ftp'
-require 'ftp_change'
 
 class FtpDeploy
 	def initialize( config = {} )
 		@config = read_config.merge( config )
+
+		@source_control = @config[:scm]
 		@ftp_connection = Net::FTP.new( @config[:host], @config[:user], @config[:password] )
 	end
 
-	def deploy
+	# Full recipe of FTP-Deployment
+	#
+	# First, get a new deployer with default options
+	# Second, find the actual remote version
+	# Third, get a list of changes since then
+	# Forth, deploy the changes in the list
+	# Fifth, ...
+	def self.do_as_you_will
+		deployer = self.new
+		remote_version = deployer.currently_deployed_version
+		changelist = deployer.changes_from_scm( :since => remote_version )
+		deployer.deploy( changelist )
+	end
+
+	def deploy( changelist = [] )
 		changelist.each do |change|
 			change.base_pathnames( @config[:local_base_path], @config[:remote_base_path] )
 			change.execute_via( @ftp_connection )
@@ -18,10 +33,7 @@ class FtpDeploy
 		{} # hash from yaml or ruby-file
 	end
 
-	def changelist
-		@changelist ||= changes_from_scm( :since => @config[:last_deployed] )
-	end
-
+	# changes_from_scm( :since => @config[:last_deployed] )
 	def changes_from_scm( options )
 		[] # changes.each { |change| FtpChange.new( change.path, change.action ) }
 	end
