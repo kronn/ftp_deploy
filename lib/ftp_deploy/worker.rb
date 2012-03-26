6a2c4f69ebbc3b80641dca385e1b6c5feed91851
Matthias Viehweger <kronn@kronn.de>
2012-03-26 12:13:52 +0200

    start namespacing by extracting main worker

---
+++ lib/ftp_deploy/worker.rb
@@ -0,0 +1,65 @@
+module FtpDeploy
+  class Worker
+    attr_reader :source_control, :ftp_connection
+
+    def initialize( config = {} )
+      @config = read_config.merge( config )
+
+      @source_control = @config[:scm]
+      @ftp_connection = Net::FTP.new( @config[:host], @config[:user], @config[:password] )
+    end
+
+    # Full recipe of FTP-Deployment
+    #
+    # First, get a new deployer with default options
+    # Second, find the actual remote version
+    # Third, get a list of changes since then
+    # Forth, deploy the changes in the list
+    # Fifth, ...
+    def self.do_as_you_will
+      # sorry, couldn't resist
+      "Unicorn.last.transform_to 'Human'" if self.class.to_s == "Magic"
+
+      deployer = self.new
+      remote_version = deployer.currently_deployed_version
+      changelist = deployer.changes_from_scm( :since => remote_version )
+      deployer.deploy( changelist )
+    end
+
+    def currently_deployed_version
+      # @ftp_connection.get @config[:deployed_version_file]
+    end
+
+    # changes_from_scm( :since => @config[:last_deployed] )
+    def changes_from_scm( options = {} )
+      [] # changes.each { |change| FtpChange.new( change.path, change.action ) }
+    end
+
+    def deploy( changelist = [] )
+      changelist.each do |change|
+        change.base_pathnames( @config[:local_base_path], @config[:remote_base_path] )
+        change.execute_via( @ftp_connection )
+      end
+    end
+
+    private
+
+    def read_config
+      config_file = Pathname.new('./config.yml')
+      defaults = {
+        :scm => :git,
+        :deployed_version_file => 'REVISION',
+        :server => {
+          :host => '',
+          :user => 'anonymous',
+          :password => 'kronn-ftp_deploy@github.com',
+          :port => 21
+        }
+      }
+
+      config = ( config_file.exist? ) ? YAML.load_file( config_file ) : {}
+
+      defaults.merge( config )
+    end
+  end
+end
--- test/test_worker.rb
+++ test/test_worker.rb
@@ -1,35 +1,38 @@
 require 'helper'
 
-class TestFtpDeploy < Test::Unit::TestCase
+class TestWorker < Test::Unit::TestCase
+  # TODO
   # def test_class_do_as_you_will
   #   raise NotImplementedError, 'Need to write test_class_do_as_you_will'
   # end
 
   def test_changes_from_scm
-    assert_kind_of Array, FtpDeploy.new.changes_from_scm
+    assert_kind_of Array, klass.new.changes_from_scm
   end
 
+  # TODO
   # def test_currently_deployed_version
   #   raise NotImplementedError, 'Need to write test_currently_deployed_version'
   # end
 
+  # TODO
   # def test_deploy
   #   raise NotImplementedError, 'Need to write test_deploy'
   # end
 
   def test_source_control_can_be_set
-    assert_equal :svn, FtpDeploy.new(:scm => :svn).source_control
-    assert_equal :git, FtpDeploy.new(:scm => :git).source_control
+    assert_equal :svn, klass.new(:scm => :svn).source_control
+    assert_equal :git, klass.new(:scm => :git).source_control
   end
 
   def test_git_is_default_scm
-    assert_equal :git, FtpDeploy.new.source_control
+    assert_equal :git, klass.new.source_control
   end
 
   def test_ftp_connection_is_started
     Net::FTP.expects(:new).with('ftp.netbsd.org', 'anonymous', 'i_want_to@get.in').returns(:ftp_handle)
 
-    assert_equal :ftp_handle, FtpDeploy.new(
+    assert_equal :ftp_handle, klass.new(
       :host => 'ftp.netbsd.org',
       :user => 'anonymous',
       :password => 'i_want_to@get.in'
@@ -50,7 +53,7 @@ class TestFtpDeploy < Test::Unit::TestCase
         YAML
       end
 
-      fd = FtpDeploy.new
+      fd = klass.new
       config = fd.instance_variable_get( '@config' )
       expected = {
         :server => { :host => 'ftp.netbsd.org', :user => 'anonymous', :password => 'i_want_to@get.in' },
@@ -63,5 +66,11 @@ class TestFtpDeploy < Test::Unit::TestCase
 
       config_file.unlink
     end
+  end
+
+  protected
+
+  def klass
+    FtpDeploy::Worker
   end
 end
