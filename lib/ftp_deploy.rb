c4dc677b8f6987fd2a53881f580ceb27b5458e24
Matthias Viehweger <kronn@kronn.de>
2012-12-16 19:38:25 +0100

    move working untested executable into bin-dir

--- lib/ftp_deploy.rb
+++ lib/ftp_deploy.rb
@@ -1,6 +1,9 @@
 require 'net/ftp'
 require 'pathname'
 
+# just to be clear, the binary does not execute any on this, yet.
+# bin and lib contain essentially different approaches, right now.
+
 module FtpDeploy
   autoload :Worker, 'ftp_deploy/worker'
   autoload :Change, 'ftp_deploy/change'
